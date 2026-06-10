8d791589606f4f576a1dd359832133307a033b25
arpangoyal01 <goyal.arpan01@gmail.com>
2026-06-10 09:26:12 +0530

    added postgreSql database of supabase

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: Number(config.get('DB_PORT')),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+
+
+        autoLoadEntities: true,
+        synchronize: true,
+
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+
+    AuthModule,
+    UsersModule,
+  ],
+})
+export class AppModule { }
