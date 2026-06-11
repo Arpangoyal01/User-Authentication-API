e7b7e15692dd35181778053b5805b6874641f1ea
arpangoyal01 <goyal.arpan01@gmail.com>
2026-06-11 12:56:14 +0530

    Prepare auth api for render

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors();
+
   app.useGlobalPipes(
   new ValidationPipe({
     whitelist: true,
   }),
 );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
 bootstrap();
